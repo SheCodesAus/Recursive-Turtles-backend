365dd5ba4fc2e2a76d39f4e305c322bb99985bb7
Anupama Kittur <anupama.kittur@gmail.com>
2026-05-04 12:23:01 +1000

    adde urls for polls, responses, results

--- workshopnav/events/urls.py
+++ workshopnav/events/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('events/', views.EventListCreateView.as_view()),
     path('events/<str:code>/', views.EventByCodeView.as_view()),
+    path('events/<int:event_id>/polls/', views.PollListCreateView.as_view()),
+    path('polls/<int:poll_id>/responses/', views.PollResponseCreateView.as_view()),
+    path('polls/<int:poll_id>/results/', views.PollResultsView.as_view()),
 ]
